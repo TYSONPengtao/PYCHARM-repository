f4528746c179b4509ee5869866c0eff1da9ea8eb
TYSONPengtao <1581845497@qq.com>
2020-07-08 02:30:44 +0800

    pyton基础语法第二次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,5 +1,10 @@
-pi=3.14
-r=eval(input("请输入圆的半径："))
-s=pi*r*r
-c=2*pi*r
-print("圆的半径为{}时，圆的面积为{},圆的周长为{}".format(r,s,c))
+#一元二次方程求解
+a=eval(input("请输入a:"))
+b=eval(input("请输入b:"))
+c=eval(input("请输入c："))
+d=b**2-4*a*c
+if d>=0:
+    x1=(-b+d**0.5)/(2*a)
+    x2=(-b-d**0.5)/(2*a)
+else:
+    print("input data error!!")
