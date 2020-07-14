7de0867b6ac0b5f49f89d5080f58f723e3dca88b
TYSONPengtao <1581845497@qq.com>
2020-07-15 00:14:29 +0800

    pyton学习第3次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,21 +1,16 @@
-print("<<<<<<<<猜数字游戏>>>>>>>>")
 import random
-my_screat=random.randint(0,10)
-def guss_number():
-    tries = 3
-    while tries>0:
-        password=input("你有3次机会，请输入你猜的数字：")
-        my_answer=int(password)
-        tries-=1
-        if my_answer==my_screat:
-            print("猜对了！！剩余次数为{}".format(tries))
-            break
-        elif my_answer<my_screat:
-            print("猜小了！！剩余次数为{}".format(tries))
-        elif my_answer>my_screat:
-            print("猜大了！！剩余次数为{}".format(tries))
-        else:
-                print("data eroor!!!")
+password=random.randint(0,10)
+tries=3
+while tries>0:
+    my_answer = int(input("请输入你猜的数字："))
+    tries-=1
+    if my_answer==password:
+        print("猜对了，剩余次数{}".format(tries))
+    elif my_answer>password:
+        print("猜大了，剩余次数{}".format(tries))
+    elif my_answer<password:
+        print("猜小了，剩余次数{}".format(tries))
     else:
-        print("游戏结束")
-guss_number()
+        print("DATA error!!!")
+else:
+    print("游戏结束！！")
