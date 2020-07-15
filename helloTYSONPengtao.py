cd7c890f524331a6c5a6b1fb2d92610e2a568d74
TYSONPengtao <1581845497@qq.com>
2020-07-15 12:07:42 +0800

    pyton学习第5次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,12 +1,7 @@
-my_job=int(input("请输入你的分数："))
-my_show=my_job/10
-if 0<=my_show<6:
-    print("D")
-elif 6<=my_show<8:
-    print("C")
-elif 8<=my_show<=9:
-     print("B")
-elif 9<my_show<=10:
-    print("A")
-else:
-    print("data error!!!")
+#从0开始，逢偶加2
+for i in range(10):
+    if i%2!=0:
+        print(i)
+        continue
+    i+=2
+    print(i)
