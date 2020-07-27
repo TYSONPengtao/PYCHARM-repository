37ea04d221b63112dab88b7941a164eabe6a5780
TYSONPengtao <1581845497@qq.com>
2020-07-27 14:42:49 +0800

    pyton学习第8次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,13 +1,4 @@
-a=input("请输入数字a:")
-b=input("请输入数字b:")
-c=input("请输入数字c:")
-print("输入顺序为：",a,b,c)
-if a<b:
-    a,b=b,a
-if a<c:
-    print("顺序为：",c,a,b)
-else:
-    if c>b:
-        print("顺序为：",a,c,b)
-    else:
-        print("顺序为：",a,b,c)
+s=0
+for i in range(1,101,2):
+    s=s+i
+print("100以内奇数和为:",s)
