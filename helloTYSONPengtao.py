ea88413425be3d6deae120db329feec486c9b785
TYSONPengtao <1581845497@qq.com>
2020-07-27 16:54:31 +0800

    pyton学习while循环第10次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,5 +1,16 @@
-s=0
-for i in range(1,101):
-    if i%2==1:
-        s=s+i
-print("100以内奇数和为:",s)
+from random import randint
+n=randint(10,100)
+tries = 3
+while tries>0:
+    my_guss = int(input("请输入你猜的价格："))
+    tries -= 1
+    if my_guss>n:
+        print("猜大了！！剩余次数为{}".format(tries))
+    elif my_guss<n:
+        print("猜小了！！剩余次数为{}".format(tries))
+    elif my_guss==n:
+        print("猜测正确！！剩余次数为{}".format(tries))
+    else:
+        print("data error")
+else:
+    print("游戏结束了！！")
