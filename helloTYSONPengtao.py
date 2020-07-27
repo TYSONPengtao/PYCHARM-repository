7fe86cae9a15eb36278471e433cb4f8a9b1e333d
TYSONPengtao <1581845497@qq.com>
2020-07-27 17:14:36 +0800

    pyton学习while循环第11次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,16 +1,15 @@
 from random import randint
 n=randint(10,100)
-tries = 3
-while tries>0:
-    my_guss = int(input("请输入你猜的价格："))
-    tries -= 1
-    if my_guss>n:
-        print("猜大了！！剩余次数为{}".format(tries))
-    elif my_guss<n:
-        print("猜小了！！剩余次数为{}".format(tries))
-    elif my_guss==n:
-        print("猜测正确！！剩余次数为{}".format(tries))
+print("请输入你猜的价格(10~100)")
+bingo=False
+while bingo==False:
+    my_guss = eval(input("请输入你猜的价格："))
+    if my_guss<n:
+        print("猜小了！！")
+    elif my_guss>n:
+        print("猜大了！！")
     else:
-        print("data error")
+        print("猜测正确！！价格是{}".format(my_guss))
+        bingo = True
 else:
-    print("游戏结束了！！")
+    print("游戏结束！！")
