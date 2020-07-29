1de85c15ef96093487d9e3a824ad06949ebeb7ff
TYSONPengtao <1581845497@qq.com>
2020-07-29 14:12:53 +0800

    pyton学习循环第13次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,7 +1,8 @@
-x=eval(input("请输入你第一个数字："))
-y=eval(input("请输入你第二个数字："))
-if x<y:
-    x,y=y,x
-for i in range(x,x*y+1):
-    if i%x==0 and i%y==0:
-        print(x,"和",y,"的最小公倍数为",i)
+s,n=0,0
+for i in range(1,11):
+    score=eval(input("请输入你的成绩："))
+    if score>=60:
+        s=s+score
+        n+=1
+print("合格人数为：",n)
+print("平均成绩为：",s/n)
