8135338455696bca91276db5f13e280b1afe0ef4
TYSONPengtao <1581845497@qq.com>
2020-07-29 23:49:51 +0800

    pyton学习循环第16次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,14 +1,7 @@
-#海伦公式
-a=eval(input("请输入a边的值："))
-b=eval(input("请输入b边的值："))
-c=eval(input("请输入c边的值："))
-if a>0 and b>0 and c>0:
-    if (b+c>a) and (a+c>b) and (b+a>c):
-        print(a,b,c,"构成三角形")
-        p=(a+b+c)/2
-        s=(p*(p-a)*p*(p-b)*p*(p-c))**0.05
-        print("三角形的面积为：",s,end="")
-    else:
-        print("输入错误！！！")
+a=eval(input("请输入一个年份："))
+if a%400==0:
+    print(a,"是闰年")
+elif a%4==0 and a%100!=0:
+    print(a, "也是闰年")
 else:
-    print("三边必须是正数")
+    print(a,"是平年")
