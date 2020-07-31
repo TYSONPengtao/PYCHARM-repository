1ca06bd6094d3a364da8f2f8daa52da39f6e7835
TYSONPengtao <1581845497@qq.com>
2020-07-31 14:26:19 +0800

    pyton学习循环第18次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,7 +1,7 @@
-a=eval(input("请输入第一个数："))
-b=eval(input("请输入第二个数："))
-if b>a:
-    a,b=b,a
-for i in range(1,b+1): #i为最大公约数
-    if a%i==0 and b%i==0:
-        print(a,b,"最大公约数为：",i)
+a=eval(input("请输入一个数："))
+for i in range(2,a):
+    if a%i==0:
+        print(a,"不是素数")
+        break
+else:
+    print(a,"是素数")
