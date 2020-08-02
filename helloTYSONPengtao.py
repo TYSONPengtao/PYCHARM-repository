0b46d46aeb58765537d61653d2012ca2cd2ddeb4
TYSONPengtao <1581845497@qq.com>
2020-08-03 02:33:56 +0800

    pyton学习第21次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,16 +1,6 @@
-n=2011
-china=13.4
-india=12.1
-bingo=False
-while bingo==False:
-    if china>=india:
-        china+=china*0.0048
-        india+=india*0.012
-        n=n+1
+def fib(n):
+    if n<2:
+        return n
     else:
-        print("印度在{}年超过中国".format(n))
-        break
-else:
-    print("计算结束")
-
-
+        return fib(n-1)+fib(n-2)
+print(fib(8))
