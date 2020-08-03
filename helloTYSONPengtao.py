612a6cd986617fc1dc593d31c7b299caf71f56b1
TYSONPengtao <1581845497@qq.com>
2020-08-03 15:15:10 +0800

    pyton学习第22次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,7 +1,7 @@
-#阶乘
-def fact(n):
-    if n==1:
+def fact_sum(n):
+    if n == 1:
         return 1
-    else:
-        return n*fact(n-1)
-print(fact(3))
+    elif n == 2:
+        return 3
+    return fact_sum(n-1) + n * (fact_sum(n-1)-fact_sum(n-2))
+print(fact_sum(30))
