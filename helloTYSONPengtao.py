34554f32ba0a4989c9ab9159ef41b93899e42948
TYSONPengtao <1581845497@qq.com>
2020-08-05 13:41:33 +0800

    pyton学习第23次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,7 +1,24 @@
-def fact_sum(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 3
-    return fact_sum(n-1) + n * (fact_sum(n-1)-fact_sum(n-2))
-print(fact_sum(30))
+class person:
+    number=0
+    def __init__(self,name,gender,age,):
+        self.name=name
+        self.gender=gender
+        self.age=age
+        person.number+=1
+    def displayperson(self):
+        print("name:",self.name,"gender:",self.gender,"age:",self.age)
+    def displaynumber(self):
+        print("total person:",person.number)
+stul=person("lingling","m",21)
+stu2=person("pengtao","h",22)
+setattr(stul,"score",90)
+setattr(stul,"score",100)
+stul.displayperson()
+stu2.displayperson()
+print("total students:",person.number)
+stul.scoure=90
+stu2.scoure=100
+print(getattr(stul,"score"))
+print("the scoure for the first students:",stul.scoure)
+delattr(stul,"score")
+print(hasattr(stul,"score"))
