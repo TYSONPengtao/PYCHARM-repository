0db2eda605ec691958103be20cf7a11e8249a54c
TYSONPengtao <1581845497@qq.com>
2020-08-05 16:54:12 +0800

    pyton学习第24次提交

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,24 +1,10 @@
+#构造函数
 class person:
-    number=0
-    def __init__(self,name,gender,age,):
+    def __init__(self,name,gender,age):
         self.name=name
         self.gender=gender
         self.age=age
-        person.number+=1
-    def displayperson(self):
-        print("name:",self.name,"gender:",self.gender,"age:",self.age)
-    def displaynumber(self):
-        print("total person:",person.number)
-stul=person("lingling","m",21)
-stu2=person("pengtao","h",22)
-setattr(stul,"score",90)
-setattr(stul,"score",100)
-stul.displayperson()
-stu2.displayperson()
-print("total students:",person.number)
-stul.scoure=90
-stu2.scoure=100
-print(getattr(stul,"score"))
-print("the scoure for the first students:",stul.scoure)
-delattr(stul,"score")
-print(hasattr(stul,"score"))
+stu1=person("lingming","m",19)
+stu2=person("zhangli","m",21)
+print("name:",stu1.name,"gender:",stu1.gender,"age:",stu1.age)
+print("name:",stu2.name,"gender:",stu2.gender,"age:",stu2.age)
