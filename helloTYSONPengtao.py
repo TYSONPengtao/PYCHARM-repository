8132fe7c931cf54fa530a0b89cd2daf4288a35f3
TYSONPengtao <1581845497@qq.com>
2020-12-18 21:09:10 +0800

    pythonn重启测试

--- helloTYSONPengtao.py
+++ helloTYSONPengtao.py
@@ -1,7 +1,1 @@
-a=eval(input("请输入第一个数："))
-b=eval(input("请输入第二个数："))
-if b>a:
-    a,b=b,a
-for i in range(1,b+1): #i为最大公约数
-    if a%i==0 and b%i==0:
-        print(a,b,"最大公约数为：",i)
+print("this is the best job for my tests")
